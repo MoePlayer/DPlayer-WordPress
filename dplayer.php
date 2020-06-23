fe19221bcd60679183acbbf641e7716bc38b96d3
Tony <lxtx.ljb@163.com>
2020-06-23 17:16:52 +0800

    Update dplayer.php
    update cdn url

--- dplayer.php
+++ dplayer.php
@@ -197,7 +197,7 @@ EOF;
             $dplayer_version = get_option( 'kblog_danmaku_dplayer_version', '1.25.0' );
             
             if ($current_time - $last_check > 86400 /* 86400 = 60 * 60 * 24 i.e 24hrs */) {
-                $response = wp_remote_get( 'https://cdnjs.cat.net/ajax/libs/dplayer/package.json' );
+                $response = wp_remote_get( 'https://cdnjs.loli.net/ajax/libs/dplayer/package.json' );
                 if ( is_array( $response ) && ! is_wp_error( $response ) ) {
                     $body = $response['body']; // use the content
                     $json_data = @json_decode($body, true);
@@ -212,8 +212,8 @@ EOF;
                 update_option( 'kblog_danmaku_dplayer_version_check', $current_time );
             }
             
-            wp_enqueue_style( 'dplayer', esc_url("https://cdnjs.cat.net/ajax/libs/dplayer/$dplayer_version/DPlayer.min.css"), false, $dplayer_version, false );
-            wp_enqueue_script( 'dplayer', esc_url("https://cdnjs.cat.net/ajax/libs/dplayer/$dplayer_version/DPlayer.min.js"), false, $dplayer_version, false );
+            wp_enqueue_style( 'dplayer', esc_url("https://cdnjs.loli.net/ajax/libs/dplayer/$dplayer_version/DPlayer.min.css"), false, $dplayer_version, false );
+            wp_enqueue_script( 'dplayer', esc_url("https://cdnjs.loli.net/ajax/libs/dplayer/$dplayer_version/DPlayer.min.js"), false, $dplayer_version, false );
             wp_enqueue_script( 'init-dplayer', plugins_url('dplayer/init-dplayer.js', __FILE__), false, '1.0.0', false );
             self::$add_script = true;
         } 
